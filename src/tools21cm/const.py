16c1945d80cde03099b733775003bb8f72dd5c2b
SimonP2207 <simon.purser@skao.int>
2023-04-17 17:26:50 +0100

    Precalculated comoving dists calculated upon module import
    - Previously precalculated comoving distances (tools21cm.cosmology.precalc_table_cdist) were hard-coded. Now they are calculated using tools21cm.cosmology.z_to_cdist which uses the cosmological parameters found in tools21cm.const.
    - All tools21cm.const.set_* methods (for updating cosmological parameters) also update tools21cm.cosmology.precalc_table_cdist values. Thus, updates to cosmological parameters should be set within python script utilising tools21cm, using those methods (set_hubble_h, set_omega_matter, set_omega_baryon, set_omega_lambda, set_ns, and set_sigma_8 of tools21cm.const)

--- src/tools21cm/const.py
+++ src/tools21cm/const.py
@@ -1,6 +1,6 @@
-'''
+"""
 The default cosmological constants in this package are the values derived from 7 year WMAP data. You can define new values using the following functions.
-'''
+"""
 
 # This file contains cosmological constants, physical constants and conversion factors.
 
@@ -27,66 +27,98 @@ solar_masses_per_gram = 5.02785431e-34
 kms = 1.e5 #1 km/s in cm/s
 
 # Cosmology
-#h = 0.7
-#Omega0 = 0.27
-#OmegaB = 0.044
-#lam = 1.0-Omega0; OmegaL = lam
-#n_s = 0.96
-#sigma_8 = 0.8
+h = 0.7
+Omega0 = 0.27
+OmegaB = 0.044
+lam = 1.0-Omega0; OmegaL = lam
+n_s = 0.96
+sigma_8 = 0.8
 
 # ES : Update for Planck 2018 for 21cmFast input
-h = 0.6766
-Omega0 = (0.02242 + 0.11933) / 0.6766 ** 2.
-OmegaB = 0.02242 / 0.6766 ** 2.
-lam = 1.0-Omega0; OmegaL = lam
-n_s = 0.9665
-sigma_8 = 0.8102
+# h = 0.6766
+# Omega0 = (0.02242 + 0.11933) / 0.6766 ** 2.
+# OmegaB = 0.02242 / 0.6766 ** 2.
+# lam = 1.0-Omega0; OmegaL = lam
+# n_s = 0.9665
+# sigma_8 = 0.8102
+
 
 # Set cosmological parameter
 def set_hubble_h(value):
-	'''
+	"""
 	Define new hubble constant value (little h).
-	'''
+	"""
+	from tools21cm import cosmology as cm
+
 	global h, H0
 	h  = value 
 	H0 = 100.0*h
 
-def set_omega_matter(value): 
-	'''
+	cm.precalc_table_cdist = cm.z_to_cdist(cm.precalc_table_z)
+	# cm.recalculate_precalc_cdists()
+
+
+def set_omega_matter(value):
+	"""
 	Define new omega matter value.
-	'''
+	"""
+	from . import cosmology as cm
+
 	global Omega0, lam
 	Omega0 = value
 	lam = 1.0-Omega0
 
+	cm.precalc_table_cdist = cm.z_to_cdist(cm.precalc_table_z)
+
+
 def set_omega_baryon(value):
-	'''
+	"""
 	Define new omega baryon value.
-	'''
+	"""
+	from . import cosmology as cm
+
 	global OmegaB
 	OmegaB = value 
 
+	cm.precalc_table_cdist = cm.z_to_cdist(cm.precalc_table_z)
+
+
 def set_omega_lambda(value):
-	'''
+	"""
 	Define new omega lambda value.
-	'''
+	"""
+	from . import cosmology as cm
+
 	global lam
 	lam = value 
 	OmegaL = value
 
+	cm.precalc_table_cdist = cm.z_to_cdist(cm.precalc_table_z)
+
+
 def set_ns(value):
-	'''
+	"""
 	Define new ns value.
-	'''
+	"""
+	from . import cosmology as cm
+
 	global n_s
 	n_s = value 
 
-def set_sigma_8(value): 
-	'''
+	cm.precalc_table_cdist = cm.z_to_cdist(cm.precalc_table_z)
+
+
+def set_sigma_8(value):
+	"""
 	Define new sigma_8 value.
-	'''
+	"""
+	from . import cosmology as cm
+
 	global sigma_8
 	sigma_8 = value 
+
+	cm.precalc_table_cdist = cm.z_to_cdist(cm.precalc_table_z)
+
 
 # Cosmology
 H0 = 100.0*h
